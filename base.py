6a8c61f5e7f43d5d0dd384da248b54caf28f5b7f
arahi10 <61438737+arahi10@users.noreply.github.com>
2022-05-18 20:31:15 +0900

    Update base.py

--- base.py
+++ base.py
@@ -23,8 +23,7 @@ def make_adjecent(i, j):
         yield (i, j+1)
     if i != MAXDAN-1:
         yield (i+1, j)
-# size (連結数)計算用の親は隣接ぷよのうちインデックスが辞書順で若い人←バグってそう、若くない人では？
-# ↑バグってませんでした
+# size (連結数)計算用の親は隣接ぷよのうちインデックスが辞書順で若くない人
 def make_children(i,j):
     if i!=0:
         yield (i-1,j)
