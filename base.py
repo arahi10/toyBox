b7328c0227b40ef6db2337af0029402df915660f
arahi10 <61438737+arahi10@users.noreply.github.com>
2022-05-18 15:11:18 +0900

    Update base.py
    バグってる箇所っぽいのを見つけた

--- base.py
+++ base.py
@@ -3,9 +3,10 @@ RED = 1
 GREEN = 2
 BLUE = 3
 YELLOW = 4
-MAXRETU = 5
+#MAXRETU = 5
+MAXRETU = 6 # 書き換え忘れ
 MAXDAN = 4
-RENSA = 5
+RENSA = 5 # 全探索領域を使い切ると最大5連鎖になるはず
 
 t = {"R":RED,"B":BLUE,"G":GREEN,"Y":YELLOW}
 
@@ -22,7 +23,7 @@ def make_adjecent(i, j):
         yield (i, j+1)
     if i != MAXDAN-1:
         yield (i+1, j)
-
+# size (連結数)計算用の親は隣接ぷよのうちインデックスが辞書順で若い人←バグってそう、若くない人では？
 def make_parent(i,j):
     if i!=0:
         yield (i-1,j)
